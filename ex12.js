e7c5b1e935238df44dd9771f0a4e2e778be54659
Asma Shahid <asmashahid795@gmail.com>
2024-03-03 01:24:04 +0500

    Add files via upload

---
+++ ex12.js
@@ -0,0 +1,5 @@
+var names = ["Ali", "Sana", "Wa", "Dia", "Shayan"];
+var messages = "welcome to this project";
+for (var i = 0; i < names.length; i++) {
+    console.log(messages + names[i]);
+}
---
+++ ex12.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Ali", "Sana", "Wa", "Dia", "Shayan"];
+const messages: string = "welcome to this project";
+for (let i = 0; i < names.length; i++) {
+  console.log(messages + names[i]);
+}
